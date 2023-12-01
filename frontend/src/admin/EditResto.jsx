a9d8d9fbb99766eb30da80f0560898c3ff3846c5
haliputri <haliputriaisyah@gmail.com>
2023-12-01 19:27:50 +0700

    edit resto sedikit

--- frontend/src/admin/EditResto.jsx
+++ frontend/src/admin/EditResto.jsx
@@ -74,8 +74,6 @@ const EditResto = () => {
         padding: '20px',
       }}
     >
-      
-
       {loading ? 
         <div className="flex flex-wrap gap-2" style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '9999' }}> 
         <Spinner color="warning" aria-label="Warning Extra large spinner example" size="xl"/> </div>
@@ -83,11 +81,11 @@ const EditResto = () => {
    
       <Card className='mt-8 mb-20' style={{ width: '100%', maxWidth: '800px', height: 'auto', textAlign: 'center' }}>
         <form className='flex flex-col gap-4'>
-        <div className='mt-20 mb-8' style={{ display: 'flex', alignItems: 'center', marginTop: '20px',width: '100%', maxWidth: '800px', height: 'auto', textAlign: 'center' }}>
-          <Button color="warning" href="/admin" outline className='text-white bg-yellow border border-transparent enabled:hover:bg-yellow-400 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900'>
+        <div className='mb-8' style={{display: 'flex', alignItems: 'center',width: '100%', maxWidth: '800px', height: 'auto', textAlign: 'center' }}>
+          <Button color="warning" href="/admin" outline className=' text-white bg-yellow border border-transparent enabled:hover:bg-yellow-400 focus:ring-4 focus:ring-yellow-300 dark:focus:ring-yellow-900'>
             <HiOutlineArrowLeft className="ml-2 h-5 w-5" />
           </Button>
-          <h1 className='text-2xl sm:text-3xl md:text-4xl ' style={{ marginLeft: '20px' }}>Edit Restaurant</h1>       
+          <h1 className='sm:text-3xl md:text-4xl ' style={{ fontSize: "20px", fontWeight: "bold" }}>Edit Restaurant</h1>       
         </div>
         <div className="relative" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <input 
