c048f40360d9a4b3f031208516652c3b0d174a57
haliputri <haliputriaisyah@gmail.com>
2023-12-15 20:27:06 +0700

    pull new

--- frontend/src/pages/user/UserRestaurantDetail.jsx
+++ frontend/src/pages/user/UserRestaurantDetail.jsx
@@ -20,35 +20,34 @@ const UserRestaurantDetail = () => {
   const [user, setUser] = useState({});
   const { username } = useParams();
 
-  const [avgRating, setAvgRating] = useState(0)
+  const [avgRating, setAvgRating] = useState(0);
 
   const [loading, setLoading] = useState(false);
   const { id } = useParams();
   const [expanded, setExpanded] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
-  const [openHour, setOpenHour] = useState('');
-  const [closeHour, setCloseHour] = useState('');
+  const [openHour, setOpenHour] = useState("");
+  const [closeHour, setCloseHour] = useState("");
   const [arrowPath, setArrowPath] = useState("M6 9l6 6 6-6");
 
   const days = [
-    { day: 'Monday', open: '11:00', close: '22:00' },
-    { day: 'Saturday', open: '11:00', close: '22:00' },
-    { day: 'Tuesday', open: '11:00', close: '22:00' },
+    { day: "Monday", open: "11:00", close: "22:00" },
+    { day: "Saturday", open: "11:00", close: "22:00" },
+    { day: "Tuesday", open: "11:00", close: "22:00" },
   ];
 
   useEffect(() => {
     const now = new Date();
-    const currentDay = now.toLocaleDateString('en-US', { weekday: 'long' });
+    const currentDay = now.toLocaleDateString("en-US", { weekday: "long" });
     const currentHour = now.getHours();
     const currentMinute = now.getMinutes();
 
     const today = days.find((item) => item.day === currentDay);
 
     if (today) {
-
-      const openTime = parseInt(today.open.split(':')[0]);
-      const closeTime = parseInt(today.close.split(':')[0]);
+      const openTime = parseInt(today.open.split(":")[0]);
+      const closeTime = parseInt(today.close.split(":")[0]);
       setOpenHour(today.open);
       setCloseHour(today.close);
 
@@ -82,7 +81,6 @@ const UserRestaurantDetail = () => {
 
     setLoading(true);
 
-
     axios
       .get(`${API_BASE_URL}/restaurants/${id}`)
       .then((restaurantResponse) => {
@@ -96,7 +94,6 @@ const UserRestaurantDetail = () => {
         setLoading(false);
       });
 
-
     axios
       .get(`${API_BASE_URL}/users/login/find/${username}`)
       .then((userResponse) => {
@@ -108,7 +105,6 @@ const UserRestaurantDetail = () => {
         console.log(error);
         setLoading(false);
       });
-
 
     axios
       .get(`${API_BASE_URL}/reviews/average-rating/${id}`)
@@ -139,7 +135,6 @@ const UserRestaurantDetail = () => {
   useEffect(() => {
     setLoading(true);
 
-
     Promise.all([axios.get(`${API_BASE_URL}/reviews/restaurant/${id}`)])
       .then(([reviewResponse]) => {
         setNewReviews(reviewResponse.data.data);
@@ -153,39 +148,40 @@ const UserRestaurantDetail = () => {
   }, [username, id]);
 
   const submitReview = async () => {
-    try{
+    try {
       await axios
-      .post(`${API_BASE_URL}/reviews`, {
-        restaurantName: restaurant.name,
-        restaurantId: restaurant._id,
-        rating: rating,
-        authorName: user.username,
-        content: review,
-        image: "",
-        authorId: user._id,
-      })
-      .then((e) => {
+        .post(`${API_BASE_URL}/reviews`, {
+          restaurantName: restaurant.name,
+          restaurantId: restaurant._id,
+          rating: rating,
+          authorName: user.username,
+          content: review,
+          image: "",
+          authorId: user._id,
+        })
+        .then((e) => {
+          const newReview = e.data.data;
+          setNewReviews([newReview, ...newReviews]);
+          setReview("");
+          setRating(0);
 
-        const newReview = e.data.data;
-        setNewReviews([newReview, ...newReviews]);
-        setReview("");
-        setRating(0);
-
-        setIsShown(false);
-        window.location.reload();
-      });
+          setIsShown(false);
+          window.location.reload();
+        });
     } catch (error) {
-      console.error('Error submitting review:', error);
+      console.error("Error submitting review:", error);
     }
   };
 
-
   const [currentPageReview, setCurrentPageReview] = useState(1);
-  const [review, setReview] = useState("")
+  const [review, setReview] = useState("");
   const reviewsPerPage = 3;
   const indexOfLastReview = currentPageReview * reviewsPerPage;
   const indexOfFirstReview = indexOfLastReview - reviewsPerPage;
-  const currentReviews = newReviews.slice(indexOfFirstReview, indexOfLastReview);
+  const currentReviews = newReviews.slice(
+    indexOfFirstReview,
+    indexOfLastReview
+  );
 
   const paginate = (pageNumber) => {
     setCurrentPageReview(pageNumber);
@@ -255,7 +251,6 @@ const UserRestaurantDetail = () => {
       arrowPath === "M6 9l6 6 6-6" ? "M18 15l-6-6-6 6" : "M6 9l6 6 6-6"
     );
   };
-
 
   return (
     <div>
@@ -459,10 +454,6 @@ const UserRestaurantDetail = () => {
       </div>
       <div>
         <div className="flex items-center justify-between mb-8 mt-20 mx-20">
-          <h2 className="text-orange-FFA90A md:text-3xl lg:text-3xl dark:text-white font-bold font-['Lato']"> Reviews </h2>
-          <div style={{ cursor: "pointer" }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#FFA90A" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" onClick={handleClick}>
-              <line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
           <h2 className="text-orange-FFA90A md:text-3xl lg:text-3xl dark:text-white font-bold font-['Lato']">
             {" "}
             Reviews{" "}
@@ -483,115 +474,197 @@ const UserRestaurantDetail = () => {
               <line x1="12" y1="5" x2="12" y2="19"></line>
               <line x1="5" y1="12" x2="19" y2="12"></line>
             </svg>
-          </div>
-        </div>
-        {isShown && (
-          <div className="mx-16 relative">
-            <textarea
-              className="inset-0 w-full mb-4 text-gray-900 bg-white border border-yellow-300 font-['Lato'] rounded-lg"
-              rows={7}
-              onChange={(e) => setReview(e.target.value)}
-            ></textarea>
-            <div className="flex space-x-2 absolute mb-6 bottom-4 left-4">
-              {renderStars()}
+            <h2 className="text-orange-FFA90A md:text-3xl lg:text-3xl dark:text-white font-bold font-['Lato']">
+              {" "}
+              Reviews{" "}
+            </h2>
+            <div style={{ cursor: "pointer" }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="28"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#FFA90A"
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                onClick={handleClick}
+              >
+                <line x1="12" y1="5" x2="12" y2="19"></line>
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+              </svg>
             </div>
-            <Button
-              className="absolute mb-6 bottom-4 right-6 rounded-md"
-              style={{
-                backgroundColor: "#FFA90A",
-                borderRadius: "50px",
-              }}
-              onClick={submitReview}
-            >
-              Submit Review
-            </Button>
-            <div className="flex items-center"></div>
           </div>
-        )}
-        <div className="mx-20 mb-4">
-           {newReviews.length === 0 ? (
-            <p className="text-lg text-center">Tidak ada reviews</p>
-          ) : (
-            currentReviews.map((review, index) => (
-              <div key={index} className="my-3">
-                <div className='inline-flex justify-start items-start'>
-                  <img src={`https://ui-avatars.com/api/?name=${review.author_name}`} className="rounded-full" alt={`Avatar of ${review.author_name}`} />
-                  <div className='flex-col justify-start items-start ml-2' style={{ width: 'calc(100% - 8px)' }}>
-                    <h5 className="text-black text-xl font-semibold font-['Lato'] m-0.5 ml-2">{review.author_name}</h5>
-                    <div className="flex items-center ml-1.5">
-                      <img src={starabu} className="w-6 h-6" alt="Star Icon" />
-                      <h5 className="text-zinc-300 text-xl font-medium font-['Lato'] ml-2">{review.review_rating}</h5>
-                    </div>
-                    <p className="ml-3 mb-2 text-black text-xl font-['Lato']">{review.review_text}</p>
-                  </div>
-                </div>
-                <div className="border border-zinc-300" style={{ width: '100%' }}></div>
+          {isShown && (
+            <div className="mx-16 relative">
+              <textarea
+                className="inset-0 w-full mb-4 text-gray-900 bg-white border border-yellow-300 font-['Lato'] rounded-lg"
+                rows={7}
+                onChange={(e) => setReview(e.target.value)}
+              ></textarea>
+              <div className="flex space-x-2 absolute mb-6 bottom-4 left-4">
+                {renderStars()}
               </div>
-            )))}
-        </div>
-        {newReviews.length > 0 && (
-          <div className="flex items-center justify-center mt-4">
-            <svg xmlns="http://www.w3.org/2000/svg"
-              width="28"
-              height="28"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke={currentPageReview > 1 ? "#FFA90A" : "#D9D9D9"}
-              strokeWidth="3"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="cursor-pointer"
-              onClick={() => paginate(currentPageReview > 1 ? currentPageReview - 1 : 1)}><path d="M19 12H6M12 5l-7 7 7 7" /></svg>
-            <div className="gap-3 inline-flex mx-2">
-              {Array.from({ length: Math.min(3, Math.ceil(newReviews.length / reviewsPerPage)) }).map((_, i) => (
-                <svg
-                  key={i}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="12"
-                  height="12"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  className={`rounded-full ${currentPageReview > 3 ? (i === 0 ? 'text-orange-FFA90A' : 'text-gray-D9D9D9') : (i === currentPageReview - 1 ? 'text-orange-FFA90A' : 'text-gray-D9D9D9')}`}
-                >
-                  <circle cx="8" cy="8" r="8" fill="currentColor" />
-                </svg>
-              ))}
+              <Button
+                className="absolute mb-6 bottom-4 right-6 rounded-md"
+                style={{
+                  backgroundColor: "#FFA90A",
+                  borderRadius: "50px",
+                }}
+                onClick={submitReview}
+              >
+                Submit Review
+              </Button>
+              <div className="flex items-center"></div>
             </div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="28"
-              height="28"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke={currentPageReview < Math.ceil(newReviews.length / reviewsPerPage) ? "#FFA90A" : "#D9D9D9"}
-              strokeWidth="3"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="cursor-pointer"
-              onClick={() => paginate(currentPageReview < Math.ceil(newReviews.length / reviewsPerPage) ? currentPageReview + 1 : currentPageReview)}
-            >
-              <path d="M5 12h13M12 5l7 7-7 7" />
-            </svg>
+          )}
+          <div className="mx-20 mb-4">
+            {newReviews.length === 0 ? (
+              <p className="text-lg text-center">Tidak ada reviews</p>
+            ) : (
+              currentReviews.map((review, index) => (
+                <div key={index} className="my-3">
+                  <div className="inline-flex justify-start items-start">
+                    <img
+                      src={`https://ui-avatars.com/api/?name=${review.author_name}`}
+                      className="rounded-full"
+                      alt={`Avatar of ${review.author_name}`}
+                    />
+                    <div
+                      className="flex-col justify-start items-start ml-2"
+                      style={{ width: "calc(100% - 8px)" }}
+                    >
+                      <h5 className="text-black text-xl font-semibold font-['Lato'] m-0.5 ml-2">
+                        {review.author_name}
+                      </h5>
+                      <div className="flex items-center ml-1.5">
+                        <img
+                          src={starabu}
+                          className="w-6 h-6"
+                          alt="Star Icon"
+                        />
+                        <h5 className="text-zinc-300 text-xl font-medium font-['Lato'] ml-2">
+                          {review.review_rating}
+                        </h5>
+                      </div>
+                      <p className="ml-3 mb-2 text-black text-xl font-['Lato']">
+                        {review.review_text}
+                      </p>
+                    </div>
+                  </div>
+                  <div
+                    className="border border-zinc-300"
+                    style={{ width: "100%" }}
+                  ></div>
+                </div>
+              ))
+            )}
           </div>
-        )}
-      </div>
-      <div className='mt-16 mx-20'>
-        <h2 className="pb-8 text-orange-FFA90A md:text-3xl lg:text-3xl dark:text-white font-bold font-['Lato']"> Locations </h2>
-        {Object.keys(restaurant).length > 0 &&
-          <div className="Maps flex items-center justify-center ">
-            <iframe
-              src={`https://maps.google.com/maps?q=${restaurant.latitude.replace(",", ".")},${restaurant.longitude.replace(",", ".")}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
-              width="800px"
-              height="300px"
-              style={{ border: "0" }}
-              allowfullscreen=""
-              loading="lazy"
-            ></iframe>
-          </div>
-        }
-      </div>
-      <div className='mt-56'>
-        <FooterResto />
+          {newReviews.length > 0 && (
+            <div className="flex items-center justify-center mt-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="28"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke={currentPageReview > 1 ? "#FFA90A" : "#D9D9D9"}
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="cursor-pointer"
+                onClick={() =>
+                  paginate(currentPageReview > 1 ? currentPageReview - 1 : 1)
+                }
+              >
+                <path d="M19 12H6M12 5l-7 7 7 7" />
+              </svg>
+              <div className="gap-3 inline-flex mx-2">
+                {Array.from({
+                  length: Math.min(
+                    3,
+                    Math.ceil(newReviews.length / reviewsPerPage)
+                  ),
+                }).map((_, i) => (
+                  <svg
+                    key={i}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    className={`rounded-full ${
+                      currentPageReview > 3
+                        ? i === 0
+                          ? "text-orange-FFA90A"
+                          : "text-gray-D9D9D9"
+                        : i === currentPageReview - 1
+                        ? "text-orange-FFA90A"
+                        : "text-gray-D9D9D9"
+                    }`}
+                  >
+                    <circle cx="8" cy="8" r="8" fill="currentColor" />
+                  </svg>
+                ))}
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="28"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke={
+                  currentPageReview <
+                  Math.ceil(newReviews.length / reviewsPerPage)
+                    ? "#FFA90A"
+                    : "#D9D9D9"
+                }
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="cursor-pointer"
+                onClick={() =>
+                  paginate(
+                    currentPageReview <
+                      Math.ceil(newReviews.length / reviewsPerPage)
+                      ? currentPageReview + 1
+                      : currentPageReview
+                  )
+                }
+              >
+                <path d="M5 12h13M12 5l7 7-7 7" />
+              </svg>
+            </div>
+          )}
+        </div>
+        <div className="mt-16 mx-20">
+          <h2 className="pb-8 text-orange-FFA90A md:text-3xl lg:text-3xl dark:text-white font-bold font-['Lato']">
+            {" "}
+            Locations{" "}
+          </h2>
+          {Object.keys(restaurant).length > 0 && (
+            <div className="Maps flex items-center justify-center ">
+              <iframe
+                src={`https://maps.google.com/maps?q=${restaurant.latitude.replace(
+                  ",",
+                  "."
+                )},${restaurant.longitude.replace(
+                  ",",
+                  "."
+                )}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+                width="800px"
+                height="300px"
+                style={{ border: "0" }}
+                allowfullscreen=""
+                loading="lazy"
+              ></iframe>
+            </div>
+          )}
+        </div>
+        <div className="mt-56">
+          <FooterResto />
+        </div>
       </div>
     </div>
   );
