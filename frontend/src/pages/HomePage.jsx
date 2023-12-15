4bfce6383212eca4b426630ff87935cfc96ec4d3
nawangilmi <nawangilmi003@gmail.com>
2023-12-15 22:18:07 +0700

    edit homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,7 +5,6 @@ import HeroLanding from "../components/HeroLanding";
 import FooterResto from "../components/FooterResto";
 import CardResto from "../components/CardResto";
 import { Button } from "flowbite-react";
-import { API_BASE_URL } from "../config.js";
 
 const HomePage = () => {
   const [restaurants, setResto] = useState([]);
