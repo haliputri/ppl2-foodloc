10ed3163f3892231b00a98d168de03dfb60202a7
haliputri <haliputriaisyah@gmail.com>
2023-12-16 09:41:11 +0700

    fix nav admin

--- frontend/src/components/NavigationAdmin.jsx
+++ frontend/src/components/NavigationAdmin.jsx
@@ -8,6 +8,7 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { API_BASE_URL } from "../../../backend/config";
 
 const NavigationAdmin = () => {
   const [user, setUser] = useState({});
